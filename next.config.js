ba06994744e1105e97f0e143931bb2956f5fd681
Karaliolios Panagiotis <karaliolios.panagiotis@gmail.com>
2025-11-04 21:16:43 +0200

    Add babel-plugin-react-compiler dependency to package.json and bun.lock

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js";
+import './src/env.js';
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -16,11 +16,11 @@ const config = {
 			{
 				// Custom R2 public domain (if configured)
 				hostname: 'storage.invest-igator.karaliolios.dev',
-				
 				protocol: 'https'
 			}
 		]
-	}
+	},
+	reactCompiler: true
 };
 
 export default config;
